8ce754e69478ecef596a2ef074dab094e23f84d6
skylerstyron <skylerstyron@gmail.com>
2022-05-02 23:45:05 -0500

    added facebook link, changed share icon

--- src/App.js
+++ src/App.js
@@ -33,7 +33,22 @@ function App() {
               backgroundColor="rgba(26,166,233,0)"
               iconSize="5"
               roundness="20%"
-              url="https://www.instagram.com/caddohunt/"
+              url="https://instagram.com/beardedarchertx"
+              target="_blank"
+              size="60"
+            />
+          </div>
+          <div className="social facebook">
+            <SocialMediaIconsReact
+              borderColor="rgba(0,0,0,0.25)"
+              borderWidth="0"
+              borderStyle="solid"
+              icon="facebook"
+              iconColor="rgba(224,224,224,1)"
+              backgroundColor="rgba(26,166,233,0)"
+              iconSize="5"
+              roundness="20%"
+              url="https://www.facebook.com/profile.php?id=100080671627172"
               target="_blank"
               size="60"
             />
